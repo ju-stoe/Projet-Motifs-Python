b7109fb2c19ecf280e46d86a4158a53c229f3fa1
Justine <justine.stoeltzlen@gmail.com>
2025-05-25 19:43:36 +0800

    fonction dessiner ajoutée

--- motifs.py
+++ motifs.py
@@ -1,2 +1,9 @@
 import turtle
-#blabla cc
+import os
+
+def dessiner(nb_cotes,taille,repetitions,angle,couleur,nom_fichier="motif.png"):
+    turtle.setup(width=800, height=800)
+    turtle.bgcolor("white")
+    turtle.speed(0)
+    turtle.hideturtle()
+    turtle.color(couleur)
