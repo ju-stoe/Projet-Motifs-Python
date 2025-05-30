6e109fd7a6d0f1f5b4fcfbd4a07b93293f4f783f
Justine <justine.stoeltzlen@gmail.com>
2025-05-30 14:17:39 +0800

    correction de bug, ajout d'un paramètre pour demander à l'utilisateur quel nom il veut donner au dessin

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import os
-from motifs import dessiner  # Ton module avec les fonctions turtle
+import time  # Pour générer un nom unique si aucun n’est fourni
+from motifs import dessiner
 
 application = Flask(__name__)
 
-# Dossier où seront stockées les images générées
 IMAGE_FOLDER = os.path.join('static', 'images')
 os.makedirs(IMAGE_FOLDER, exist_ok=True)
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
+    error = None
+    image_file = None
+
     if request.method == 'POST':
-        error = None
-        image_file = None
         type_motif = request.form.get('type_motif')
         couleur = request.form.get('couleur', 'black')
+        nom_fichier = request.form.get('nom_fichier', '').strip()
+
+        # Génère un nom de fichier unique si aucun n'est fourni
+        if not nom_fichier:
+            nom_fichier = f"{type_motif}_{int(time.time())}"
+
+        filename = f"{nom_fichier}.png"
+        fichier = os.path.join(IMAGE_FOLDER, filename)
 
         try:
             if type_motif in ['polygone', 'spirale']:
@@ -23,20 +32,15 @@ def index():
                 repetitions = int(request.form.get('repetitions'))
                 angle = int(request.form.get('angle'))
 
-                filename = f"{type_motif}.png"
-                filepath = os.path.join(IMAGE_FOLDER, filename)
-                dessiner(type_motif, cotes, taille, repetitions, angle, couleur, filepath)
-                return render_template('index.html', image_file=filename, request=request)
+                dessiner(type_motif, cotes, taille, repetitions, angle, couleur, fichier)
+                image_file = filename
 
             elif type_motif == 'fractale':
                 taille = int(request.form.get('taille_fractale'))
                 niveau = int(request.form.get('niveau'))
 
-                filename = "fractale.png"
-                filepath = os.path.join(IMAGE_FOLDER, filename)
-                dessiner(type_motif, 0, taille, niveau, 0, couleur, filepath)
-                return render_template('index.html', image_file=filename, request=request)
-
+                dessiner(type_motif, 0, taille, niveau, 0, couleur, fichier)
+                image_file = filename
 
             else:
                 error = "Motif inconnu"
@@ -44,12 +48,7 @@ def index():
         except (ValueError, TypeError):
             error = "Erreur d'entrée"
 
-        # Si erreur, on affiche directement la page sans redirection
-        return render_template('index.html', error=error)
-
-    # Méthode GET – récupération du résultat via paramètre d’URL
-    image_file = request.args.get('image_file')
-    return render_template('index.html', image_file=image_file)
+    return render_template('index.html', error=error, image_file=image_file)
 
 if __name__ == '__main__':
     application.run(debug=True)
