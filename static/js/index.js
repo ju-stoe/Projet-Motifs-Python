f1cc601fdfd092d773962835fabc911eb39ab4bb
Justine <justine.stoeltzlen@gmail.com>
2025-05-30 18:17:40 +0800

    améliorations du html et css, séparation du html et js

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+function updateForm() {
+            const type = document.getElementById('type_motif').value;
+            document.getElementById('params_polygone_spirale').style.display = 'none';
+            document.getElementById('params_fractale').style.display = 'none';
+
+            if (type === 'polygone' || type === 'spirale' || type === 'cercle') {
+                document.getElementById('params_polygone_spirale').style.display = 'block';
+            } else if (type === 'fractale') {
+                document.getElementById('params_fractale').style.display = 'block';
+            }
+        }
+
+        window.onload = updateForm;
